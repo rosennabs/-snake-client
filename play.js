fddabcb17eafc7a55ae6f9c16e2065f0de754485
Rose Francis <rosennabs@gmail.com>
2023-07-14 02:22:27 -0600

    set up stdin in play.js

--- play.js
+++ play.js
@@ -1,1 +1,24 @@
+const { Socket } = require('net');
 const { connect } = require('./client');
+const { stdin } = require('process');
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+//terminate the game with ctrl+c
+const handleUserInput = function () {
+  if ("\u0003") {//character reps ctrl+c
+    console.log("Game terminated!");
+    process.exit();
+  }
+};
+
+setupInput();
